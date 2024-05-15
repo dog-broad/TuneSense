71c47dbad4b60df91718d1bb75c730b531537623
Dog-Broad <rushyendra.7004@gmail.com>
2024-05-15 21:44:16 +0530

    Update .gitignore and enhance Clean_MP3_Files.ps1 script
    - Added `foo.txt` to `.gitignore` for exclusion from version control.
    - Modified `Clean_MP3_Files.ps1` to:
      - Allow use of the current directory by default.
      - Track and display the progress of file processing within each folder.
      - Provide output for files not requiring renaming.

--- Clean_MP3_Files.ps1
+++ Clean_MP3_Files.ps1
@@ -1,5 +1,8 @@
-# Define the root directory containing the subfolders
-$rootDir = "D:\TuneSense"
+# Define the root directory containing the subfolders, use this if you want a fixed directory
+#$rootDir = "D:\TuneSense"
+
+# Uncomment the following line and comment above line if you want to use current directory
+$rootDir = "."
 
 # Define the subfolders
 $folders = @("Telugu_Mass", "Telugu_Melodies", "Telugu_Sad", "Telugu_Upbeat")
@@ -15,8 +18,16 @@ foreach ($folder in $folders) {
     # Get all mp3 files in the current folder
     $files = Get-ChildItem -Path $currentFolder -Filter "*.mp3"
 
+    # Count the number of files
+    $fileCount = $files.Count
+
+    # Current file count variable
+    $currentFileCount = 0
+
     # Loop through each file
     foreach ($file in $files) {
+        # Increment the current file count
+        $currentFileCount++
         # Check if the file name contains the string to remove
         if ($file.Name.Contains($removeString)) {
             # Create the new file name by removing the string
@@ -29,7 +40,11 @@ foreach ($folder in $folders) {
             Rename-Item -Path $file.FullName -NewName $newFilePath
 
             # Output the renamed file
-            Write-Output "Renamed '$($file.FullName)' to '$newFilePath'"
+            Write-Output "('$folder': $currentFileCount/$fileCount) 'Renamed '$($file.FullName)' to '$newFilePath'"
+        }
+        else {
+            # Output the original file name
+            Write-Output "('$folder': $currentFileCount/$fileCount) File '$($file.FullName)' does not contain the string to remove"
         }
     }
 }
